962cf0efb2997dde526e5f13d55188a038086624
victorvrin <victor.vrinceanu@gmail.com>
2021-05-11 15:53:08 +0300

    test speed for all browser types

--- context_vs_browser_vs_page_times.js
+++ context_vs_browser_vs_page_times.js
@@ -1,36 +1,38 @@
-import { chromium } from "playwright";
+import { chromium, webkit, firefox } from "playwright";
 
 (async() => {
     
-    let startTime = Date.now();
     let tries = 10;
+    let startTime;
+    let elapsed;
 
-    for (let i = 0; i < tries; i++) {
-        const browser = await chromium.launch();
+    for (const browserType of [chromium, webkit, firefox]) {
+
+        startTime = Date.now();
+        for (let i = 0; i < tries; i++) {
+            const browser = await browserType.launch();
+        }
+        console.log(`It took ${Date.now() - startTime} ms to launch ${tries} ${browserType.name()} browsers`);
+
+        // Measure Contexts launch time
+        const browser = await browserType.launch();
+
+        startTime = Date.now();
+        for (let i = 0; i < tries; i++) {
+            const context = await browser.newContext();
+        }
+        console.log(`It took ${Date.now() - startTime} ms to launch ${tries} ${browserType.name()} contexts`);
+
+        // Measure Page launch time
+        const context = await (await browser).newContext();
+
+        startTime = Date.now();
+        for (let i = 0; i < tries; i++) {
+            const page = await context.newPage();
+        }
+        console.log(`It took ${Date.now() - startTime} ms to launch ${tries} ${browserType.name()} pages`);
+
+        await context.close();
+        await browser.close();
     }
-    
-    let elapsedForBrowsers = Date.now() - startTime;
-
-    console.log(`It took ${elapsedForBrowsers} ms to launch ${tries} browsers`);
-
-    const browser = await chromium.launch();
-    for (let i = 0; i < tries; i++) {
-        const context = await browser.newContext();
-    }
-
-    let elapsedForContexts = Date.now() - startTime - elapsedForBrowsers;
-
-    console.log(`It took ${elapsedForContexts} ms to launch ${tries} contexts`);
-
-    const context = await (await browser).newContext();
-    for (let i = 0; i < tries; i++) {
-        const page = await context.newPage();
-    }
-
-    let elapsedForPages = Date.now() - startTime - elapsedForBrowsers - elapsedForContexts;
-
-    console.log(`It took ${elapsedForPages} ms to launch ${tries} pages`);
-
-    await context.close();
-    await browser.close();
 })();
